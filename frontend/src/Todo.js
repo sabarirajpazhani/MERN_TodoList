d374cf5765fb52e45f909a002fda5fde55a71134
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-05 07:45:02 +0530

    create items

--- frontend/src/Todo.js
+++ frontend/src/Todo.js
@@ -1,8 +1,19 @@
 export default function Todo(){
+
     return <>
         <div className="row p-3 bg-success text-light">
             <h1>TODO Project with MERN stack</h1>
         </div>
-        
+        <div className="row">
+            <h3>Add Items</h3>
+            <p className="text-success">Item added successfully</p>
+            <div className="form-group d-flex gap-2">
+                <input placeholder="text" className="form-control" type="text" />
+                <input placeholder="description" className="form-control" type="text" />
+                <button className="btn btn-dark">Submit</button>
+
+            </div>
+        </div>
+
     </>
 }
