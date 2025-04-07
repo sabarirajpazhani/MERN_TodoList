f72e09291b7cac7b8c4d359b657ce0f71d0951b5
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-07 07:00:20 +0530

    edit and update items

--- frontend/src/Todo.js
+++ frontend/src/Todo.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Todo(){
 
@@ -7,6 +7,11 @@ export default function Todo(){
     const [todos, setTodos] = useState([])
     const [error, setError] = useState("")
     const [message, setMessage] = useState("")
+    const [editID, setEditID] = useState(-1)
+
+    //edit
+    const [editTitle, setEditTitle] = useState("")
+    const [editDescription, setEditDescription] = useState("")
 
     const apiUrl = "http://localhost:8000"
 
@@ -38,6 +43,73 @@ export default function Todo(){
             
         }
     }
+
+    useEffect(()=>{
+        getItems()
+    },[])
+
+    const getItems = () =>{
+        fetch(apiUrl+"/todos")
+        .then((res)=>{
+            return res.json()
+        })
+        .then((res)=>{
+            setTodos(res)
+        })
+    }
+
+    const handleEdit = (item) =>{
+        setEditID (item._id); 
+        setEditTitle(item.title); 
+        setEditDescription(item.description)
+    }
+
+    const handleUpdate = () =>{
+        setError("")
+        if(editTitle.trim()!=='' && editDescription.trim()!==''){
+            fetch(apiUrl+"/todos/"+editID,{
+                method:"PUT",
+                headers:{
+                    'Content-Type' : 'application/json',
+                },
+                body:JSON.stringify({title : editTitle, description : editDescription})
+            }).then((res)=>{
+                if(res.ok){
+                    //update items to list
+                    const updatedTodos = todos.map((item) =>{
+                        if(item.id === editID){
+                            item.title = editTitle
+                            item.description = editDescription
+                        }
+                        return item
+                    })
+
+
+                    setTodos(updatedTodos)
+                    setMessage("Item updated successfully")
+                    setTimeout(()=>{
+                        setMessage("")
+                    },3000)
+                    
+                    setEditID(-1)
+                }
+                else{
+                    setError("Unable to create Todo item")
+                }
+            }).catch(()=>{
+                setError("Unable to create Todo item")
+            })
+
+
+            
+        }
+    }
+
+    const handleEditCancle = () =>{
+        setEditID(-1)
+    }
+
+
     return <>
         <div className="row p-3 bg-success text-light">
             <h1>TODO Project with MERN stack</h1>
@@ -51,6 +123,37 @@ export default function Todo(){
                 <button className="btn btn-dark"  onClick={handleSubmit}>Submit</button>
             </div>
             {error && <p className="text-danger">{error}</p>}
+        </div>
+
+        <div className="row mt-3">
+            <h3>Tasks</h3>
+            <ul className="list-group">
+                {
+                    todos.map((item)=>
+                        <li className="list-group-item bg-info d-flex justify-content-between align-items-center my-2">
+                            <div className="d-flex flex-column me-2">
+                                {
+                                    editID === -1  || editID !== item._id? <>
+                                        <span className="fw-bold">{item.title}</span>
+                                        <span >{item.description}</span>
+                                    </> : <>
+                                    <div className="form-group d-flex gap-2">
+                                        <input placeholder="text" onChange={(e)=>{setEditTitle(e.target.value)}} value={editTitle} className="form-control" type="text" />
+                                        <input placeholder="description" onChange={(e)=>{setEditDescription(e.target.value)}} value={editDescription} className="form-control" type="text" />
+                                    </div>
+                                    </>
+                                }
+                                
+                            </div>
+                            <div className="d-flex gap-2">
+                                {editID ===-1 || editID !== item._id ? <button className="btn btn-warning" onClick={() => handleEdit(item)}>Edit</button> : <button className="btn btn-warning" onClick={handleUpdate}>Update</button>}
+                                {editID ===-1 ? <button className="btn btn-danger">Delete</button> :
+                                <button className="btn btn-danger" onClick={handleEditCancle}>Cancel</button> }
+                            </div>
+                        </li>
+                    )
+                }
+            </ul>
         </div>
 
     </>
